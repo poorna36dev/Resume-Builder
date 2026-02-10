c65f66ba7593dbcef81de38b888560ad7310c7fa
poorna36dev <poorna2429@gmail.com>
2026-02-10 23:54:27 +0530

    backend completed

--- src/main/java/com/svu/resume/service/EmailService.java
+++ src/main/java/com/svu/resume/service/EmailService.java
@@ -1,5 +1,6 @@
 package com.svu.resume.service;
 
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,16 @@ public class EmailService  {
         helper.setTo(to);
         helper.setSubject(subject);
         helper.setText(htmlContent,true);
+        mailSender.send(message);
+    }
+    public void sendEmailWithAttachment(String to,String subject,String body,byte[] attachment,String filename) throws MessagingException{
+        MimeMessage message=mailSender.createMimeMessage();
+        MimeMessageHelper helper=new MimeMessageHelper(message,true);
+        helper.setFrom(fromEmail);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(body);
+        helper.addAttachment(filename,new ByteArrayResource(attachment));
         mailSender.send(message);
     }
 }
